3c2e8879f8981ce8544af21d49d40367e8bb84da
Nandhan4677 <chapalasa.nandhan@cbre.com>
2022-07-04 17:51:55 +0530

    added  ddos and chsanged resource group

--- IaC terraform/main.tf
+++ IaC terraform/main.tf
@@ -209,8 +209,8 @@ resource "azurerm_network_ddos_protection_plan" "ddos-plan" {
 
 resource "azurerm_storage_account" "storage-acc-for-vnet" {
   name                     = "${var.storage-account-for-vnet}"
-  resource_group_name      = azurerm_resource_group.azurg.name
-  location                 = azurerm_resource_group.azurg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
